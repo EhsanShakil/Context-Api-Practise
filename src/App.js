ede2625c7fc2c045ccfe33caaaecce304786b5a1
Ahsan Shakeel <ahsanshakeel1994@gmail.com>
2020-06-19 22:38:44 +0500

    Making Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
-
+import Parent from "./Praent";
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>App Component</h1>
+      <Parent />
     </div>
   );
 }
